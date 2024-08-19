477ea5fff7c047dc442ec7095c73fd0fbba1d094
Anyelo Nuñez Obispo <anyelonuob2004@gmail.com>
2024-08-19 16:40:50 -0500

    Primer commit

---
+++ src/main/java/com/skydev/ejemplo2_springdatajpa/Ejemplo2SpringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.skydev.ejemplo2_springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo2SpringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo2SpringdatajpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skydev/ejemplo2_springdatajpa/Ejemplo2SpringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skydev.ejemplo2_springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo2SpringdatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
